7eb094525ba98c505b7c81f092b3846997a84aa1
alessandro <sorrentinoalessandro02@gmail.com>
2022-11-23 12:53:11 +0100

    first commit

---
+++ src/main/java/com/corso/ProjectGLO/ProjectGloApplication.java
@@ -0,0 +1,13 @@
+package com.corso.ProjectGLO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corso/ProjectGLO/ProjectGloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corso.ProjectGLO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
