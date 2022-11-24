24aa64781b7715374b1391043083bcc149f67ff0
alessandro <sorrentinoalessandro02@gmail.com>
2022-11-24 10:55:53 +0100

    first commit

---
+++ src/main/java/com/corso/ProjectGLO/model/EmailDiNotifica.java
@@ -0,0 +1,19 @@
+package com.corso.ProjectGLO.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDiNotifica {
+
+    private String soggetto;
+    private String recipiente;
+    private String corpo;
+
+}
