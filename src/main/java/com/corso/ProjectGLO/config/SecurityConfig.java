2f24b37198a24e87775fe0b31d3b6d9d75c83b77
alessandro <sorrentinoalessandro02@gmail.com>
2022-11-28 11:07:29 +0100

    Aggiunta la parte di SecurityConfig

--- src/main/java/com/corso/ProjectGLO/config/SecurityConfig.java
+++ src/main/java/com/corso/ProjectGLO/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.corso.ProjectGLO.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,7 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated(); // // csrf utiliza i cookie per l'autenticazione
         // antMatchers autorizzi le richieste dell'url passato come parametro: da questo pattern, tutte le richieste dopo auth/ vanno bene
+    }
 
+    @Bean
+    PasswordEncoder passwordEncoder() { // per criptare la password
+        return new BCryptPasswordEncoder();
     }
 
 }
