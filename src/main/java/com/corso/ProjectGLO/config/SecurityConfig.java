fe8ebb5711e648aa5e06fc89e891ff8aeb866811
alessandro <sorrentinoalessandro02@gmail.com>
2022-11-30 16:22:12 +0100

    Modificata la classe SecurityConfig

--- src/main/java/com/corso/ProjectGLO/config/SecurityConfig.java
+++ src/main/java/com/corso/ProjectGLO/config/SecurityConfig.java
@@ -1,19 +1,21 @@
 package com.corso.ProjectGLO.config;
 
+import com.corso.ProjectGLO.model.Utente;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.thymeleaf.TemplateEngine;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public void Configure(HttpSecurity httpSecurity) throws Exception {
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf()
-                .disable()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
                 .permitAll()
@@ -28,4 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    TemplateEngine templateEngine() {
+        return new TemplateEngine();
+    }
 }
